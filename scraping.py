8755d0146d4525c5fbe86c6e18012047f7148b8e
nsmeltz <smeltznatalie@gmail.com>
2022-02-16 14:23:16 -0600

    update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,10 +1,30 @@
-
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
-import requests
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 
 # 1. Mars Nasa News Site Article Scraping 
@@ -67,9 +87,6 @@ def featured_image(browser):
 
     return img_url
 
-# Close down browser
-browser.quit()
-
 # 3. Mars Facts Table Scrape
 
 def mars_facts():
@@ -85,9 +102,13 @@ def mars_facts():
     df.columns=['Description', 'Mars', 'Earth']
     df.set_index('Description', inplace=True)
 
-    # Convert dataframe back into HTML format
-    return df.to_html()
+    # Convert dataframe into HTML format, add bootstrap
+    return df.to_html(classes="table table-striped")
 
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
 
 
 
